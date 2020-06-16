5bc77c0e07d3cd592712791cc407f32bd30b7a35
KEVIN_NOGUEIRA <kevinnog@outlook.com.br>
2020-06-15 22:23:26 -0300

    Frontend of the app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
+// Components
+import Routes from "./routes";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hellow World</h1>
-      </header>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
