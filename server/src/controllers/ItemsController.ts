ed685636a8bb334620fbda4efe6ff267f31545fe
KEVIN_NOGUEIRA <kevinnog@outlook.com.br>
2020-06-16 22:40:18 -0300

    App Mobile version - Part1

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -8,12 +8,15 @@ class ItemsController {
     const serializedItems = items.map((item) => {
       return {
         ...item,
-        image_url: `http://localhost:3333/uploads/${item.image}`,
+        image_url: `http://192.168.15.24:3333/uploads/${item.image}`,
       };
     });
 
     return response.json(serializedItems);
   }
 }
+
+// http://192.168.15.24:3333/uploads/${item.image}
+// http://localhost:3333/uploads/${item.image}
 
 export default ItemsController;
